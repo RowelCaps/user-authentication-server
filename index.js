001288f8ee4c0baf7726240cee1c41f437d5f3b6
Rowel Capulong <capulong.rowel@gmail.com>
2024-01-25 00:59:38 +0800

    Cors update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://rowelcaps.github.io',
     methods: "GET, POST, PUT, DELETE",
     credentials: true
 }));
